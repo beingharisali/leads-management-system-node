8ba0999f6d619130e4b5c6d4c8cc59920d22978a
Maaz Hashmi <maazhashmi996-pixel>
2025-12-24 14:43:10 +0500

    Task 9: Admin get all leads endpoint added

--- routes/leads.js
+++ routes/leads.js
@@ -9,29 +9,21 @@ const {
 	updateLead,
 	deleteLead,
 	updateLeadStatus,
+	getAllLeads,
 } = require("../controllers/leads");
 
 const authenticateUser = require("../middleware/authentication");
 
-
+// CSR routes
 router.get("/get-leads", authenticateUser, getLeads);
-
-
 router.get("/get-leads-by-date", authenticateUser, getLeadsByDate);
-
-
-router.post("/create-leads", authenticateUser, createLead);
-
-
 router.get("/get-single-leads/:id", authenticateUser, getSingleLead);
-
-
+router.post("/create-leads", authenticateUser, createLead);
 router.patch("/update-leads/:id", authenticateUser, updateLead);
-
-
-router.patch("/update-lead-status/:id", authenticateUser, updateLeadStatus);
-
-
+router.patch("/update-leads-status/:id", authenticateUser, updateLeadStatus);
 router.delete("/delete-leads/:id", authenticateUser, deleteLead);
+
+
+router.get("/admin/get-all-leads", authenticateUser, getAllLeads);
 
 module.exports = router;
